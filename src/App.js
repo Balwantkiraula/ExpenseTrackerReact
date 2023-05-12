aa696a128c358c032cf6d1b60e4e6845abf213f1
balwant kiraula <balwantkiraula123@gmail.com>
2023-05-13 00:27:20 +0530

    Creating Signup Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  const signUpHandle = () =>{
+
+  }
+  return (
+ <Login signUpHandle={signUpHandle}/>
+  );
+}
+
+export default App;
